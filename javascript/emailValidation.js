35a3bab6d8b31f97332cca83954a666e328a774e
Benny Teh <bennyteh98@gmail.com>
2020-12-18 23:45:46 +0800

    updated javascript function

--- javascript/emailValidation.js
+++ javascript/emailValidation.js
@@ -1,8 +1,16 @@
 const form = document.getElementById('form');
-const email = document.getElementById('email').value;
+const email = document.getElementById('email');
 const text = document.getElementById('text');
-const pattern = /^[^ ]+@[^ ]+\.[A-Z]{2,3}$/;
+const pattern = /^[^ ]+@[^ ]+\.[A-Z]{2,3}$/i;
+
 
 form.addEventListener('submit',(e) => {
-    e.preventDefault();
+    if ((email.value).match(pattern) === null){
+        text.innerHTML = "Please provide a valid email";
+        e.preventDefault();
+        email.classList.add("op-1");
+        email.classList.add("warning")
+        email.classList.remove("op-0-4");
+    }
+    
 })
